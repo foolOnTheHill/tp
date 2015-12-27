719b96a693df9dcc73a2aff4f99ab6e8aacd4bea
George Oliveira <ghao@cin.ufpe.br>
2015-12-26 23:33:03 -0300

    more methods

--- ipmt/src/SuffixTree.cpp
+++ ipmt/src/SuffixTree.cpp
@@ -68,23 +68,72 @@ public:
   }
 
   void prepareOutEdges() {
-    // TODO
+    outEdges = new list<char>[count + 1];
+
+    map< pair<int,char> , int >::iterator it;
+    for(it = edgesMap.begin(); it != edgesMap.end(); it++){
+      Edge edge = edges[it->second];
+      outEdges[edge.startNode].push_back(str[edge.l]);
+    }
   }
 
   void init() {
-    // TODO
+    count = 0;
+    stringLen = str.size();
+
+    nodes = new Node[2*stringLen + 10];
+    edges = new Edge[2*stringLen + 10];
+
+    edgesMap.clear();
+
+    root = Node(count++, -1);
+    addNode(root);
   }
 
   void buildTree() {
-    // TODO
+    init();
+
+    Suffix sf(root.id, 0, -1);
+    for (int i = 0; i < stringLen; i++) {
+      sf = update(sf, i);
+      sf = canonize(sf);
+    }
+
+    prepareOutEdges();
   }
 
   void getRepr(string &buffer) {
-    // TODO
+    buffer = "";
+
+    ostringstream os;
+    os << count << "\n";
+
+    for (int id = 1; id < count; id++) {
+      Edge e = edges[id];
+      os << e.l << "\n";
+      os << e.r << "\n";
+      os << e.startNode << "\n";
+      os << e.endNode << "\n";
+    }
+    buffer = os.str();
   }
 
   void loadRepr(string &buffer) {
-    // TODO
+    init();
+
+    stringstream ss(buffer);
+    ss >> count;
+
+    outEdges = new list<char>[count + 1];
+    for (int id = 1; id < count; id++) {
+      Edge e;
+      ss >> e.l;
+      ss >> e.r;
+      ss >> e.startNode;
+      ss >> e.endNode;
+      addEdge(e);
+      outEdges[e.startNode].push_back(str[e.l]);
+    }
   }
 
   SuffixTree(const string &s) {
