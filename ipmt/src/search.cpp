7146628e7710ddacc4e3701bc4f6f715f3ecab37
George Oliveira <ghao@cin.ufpe.br>
2015-12-27 10:40:06 -0300

    compress

--- ipmt/src/search.cpp
+++ ipmt/src/search.cpp
@@ -24,8 +24,44 @@ void output(string &filename, string &output) {
   ofs.close();
 }
 
+void compress(const string &text, string &ret) {
+  vector<int> comp_text;
+  map<string, int> dict;
+
+  for (int i = 0; i < ALPHABET_SIZE; i++) {
+    dict[string(1, i)] = i;
+  }
+
+  string aux = "";
+  char chr;
+
+  for (int i = 0; i < text.length(); i++) {
+    chr = text[i];
+
+    string tmp = aux + chr;
+
+    if (dict.count(tmp)) {
+      aux = tmp;
+    } else {
+      comp_text.push_back(dict[aux]);
+      dict[tmp] = dict.size() + 1;
+      aux = chr;
+    }
+  }
+
+  if (!aux.empty()) {
+    comp_text.push_back(dict[aux]);
+  }
+
+  ostringstream os;
+	vector<int>::iterator it;
+	for (it = comp_text.begin() ; it != comp_text.end(); ++it) {
+		os << *it << '\n';
+	}
+	ret = os.str();
+}
+
 // TODO
-void compress(const string &text, string &ret);
 void decompress(string &comp_text, string &ret);
 
 void generateIndexTree(string &textfile);
